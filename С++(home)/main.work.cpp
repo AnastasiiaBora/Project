0e48ae997f7496fd5120ff35cc381df3ee3b4fc3
AnastasiiaBora <bo.anastasiia05@gmail.com>
2023-07-05 17:15:36 +0300

    go

--- С++(home)/main.work.cpp
+++ С++(home)/main.work.cpp
@@ -3,63 +3,88 @@
 class Money
 {
 public:
-    long usd;
-    int penny;
+    long usd; // here
+    int penny; // here
  
     Money(long usd, int penny)
     {
-        this->usd = usd;
-        if (penny < 0 || penny >= 100) {
+        if (penny < 0 || usd < 0) {
             throw std::string("Wrong money!");
         }
+        // convert penny to usd if needed
+        if (penny >= 100) {
+            // 125 | 125 / 100 = 1 | 125 % 100 = 25
+            long pennyToUsd = penny / 100;
+            int newPenny = penny % 100;
+            usd += pennyToUsd;
+            penny = newPenny;
+        }
+        
+        this->usd = usd;
         this->penny = penny;
     }
-   
+    // 99 + 99 = 198
     void add (Money money2)
     {
-         this->usd = this->usd + money2.usd;
-         this->penny = this->penny + money2.penny;
-         this->calculateMoney();
+        this->usd = this->usd + money2.usd;
+        this->penny = this->penny + money2.penny;
+        this->calculateMoney();
+    }
+    
+     void compare (Money money2) {
+        if (this->usd > money2.usd && this->penny > money2.penny) {
+            std::cout << this->usd<< std::endl;
+        } else {
+            std::cout << money2.usd<< std::endl;
+        } 
     }
 
-    void subtract(Money money2)
+    void subtract (Money money2)
     {
-         this->usd = this->usd + money2.usd;
-         this->penny = this->penny + money2.penny;
-         this->calculateMoney();
+        this->usd = this->usd - money2.usd;
+        this->penny = this->penny - money2.penny;
+        this->calculateMoney();
     }
-    void calculateMoney()
+   
+ 
+    void calculateMoney ()
     {
-        if (this->penny > 100) {
-
+        if (this->penny >= 100) {
+            this->usd++;
+            this->penny -= 100;
         } else if (this->penny < 0) {
-
+            this->penny += 100;
+            if (this->usd >= 1) {
+                this->usd--;
+            } else {
+                throw std::string("Not enough money");
+            }
         }
     }
-    void print() {
+ 
+    void print ()
+    {
         std::cout << this->usd << ',' << this->penny << std::endl;
     }
 };
  
 int main() {
     try {
-        Money money1((long)10, 10000);
+        Money money1((long)1, 50);
+        money1.print();
+ 
+        Money money2((long)1, 99);
+        money2.print();
+ 
+        money1.subtract(money2);
+        std::cout << "Result: " << std::endl;
+        money1.print();
+ 
+ 
     } catch (std::string exception) {
         std::cout << exception << std::endl;
         return 0;
     }
-   // Money money2(10, 50);
-    //money1.print();
  
-    Money money2((long)5, 25);
-    money2.print();
-    
-    //money1.add(money2);
-    //money1.print();
-
-    //money1.subtract(money2);
-   // money1.print();
-    
-  return 0;
+    return 0;
 }
-
---
+++ С++(home)/main6.2.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+
+void encode_char(const char character, bool bits[8]) {
+    for (int i = 0; i < 8; i++) {
+        bits[i] = (character >> (7 - i)) & 1;
+    }
+}
+
+void encode_string(const char* string, bool bytes[][8]) {
+    int len = strlen(string);
+    for (int i = 0; i < len; i++) {
+        encode_char(string[i], bytes[i]);
+    }
+    // Set the terminator byte
+    encode_char('\0', bytes[len]);
+}
+
+char decode_byte(const bool bits[8]) {
+    char character = 0;
+    for (int i = 0; i < 8; i++) {
+        character |= (bits[i] << (7 - i));
+    }
+    return character;
+}
+
+void decode_bytes(const int rows, const bool bytes[][8], char* string) {
+    for (int i = 0; i < rows; i++) {
+        string[i] = decode_byte(bytes[i]);
+    }
+    // Add the terminator character
+    string[rows] = '\0';
+}
+
+int main() {
+    const char* text = "Hello, how are you?";
+    const int len = strlen(text);
+    bool bytes1[len + 1][8];
+    encode_string(text, bytes1);
+    for (int j = 0; j <= len; j++) {
+        printf("%c: ", text[j]);
+        for (int i = 0; i < 8; i++) {
+            printf("%d", bytes1[j][i]);
+        }
+        printf("\n");
+    }
+
+    bool bytes2[7][8] = {
+        {0, 1, 0, 0, 1, 0, 0, 0},
+        {0, 1, 1, 0, 0, 1, 0, 1},
+        {0, 1, 1, 0, 1, 1, 0, 0},
+        {0, 1, 1, 0, 1, 1, 0, 0},
+        {0, 1, 1, 0, 1, 1, 1, 1},
+        {0, 0, 1, 0, 0, 0, 0, 1},
+        {0, 0, 0, 0, 0, 0, 0, 0}
+    };
+    char string[7];
+    decode_bytes(7, bytes2, string);
+    printf("%s\n", string);
+
+    return 0;
+}
